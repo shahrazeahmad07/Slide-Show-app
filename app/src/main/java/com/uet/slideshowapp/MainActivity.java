caf37e69accbbc4d6e019cc64a97ded2d3a122a4
Shahzaib Ahmad <68849516+shahrazeahmad07@users.noreply.github.com>
2021-08-05 16:47:19 +0500

    run 2

--- app/src/main/java/com/uet/slideshowapp/MainActivity.java
+++ app/src/main/java/com/uet/slideshowapp/MainActivity.java
@@ -1,9 +1,14 @@
 package com.uet.slideshowapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,5 +33,21 @@ public class MainActivity extends AppCompatActivity {
 
         viewPager2.setAdapter(new SliderAdapter(sliderItems, viewPager2));
 
+        viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
+        viewPager2.setOffscreenPageLimit(3);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float r = 1 - Math.abs(position);
+                page.setScaleY(0.85f + r * 0.15f);
+            }
+        });
+
+        viewPager2.setPageTransformer(compositePageTransformer);
     }
 }
