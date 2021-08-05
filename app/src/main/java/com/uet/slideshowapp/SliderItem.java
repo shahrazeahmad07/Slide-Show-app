2c31a9266933b4f8b6fc0986cb215043044a0fd3
Shahzaib Ahmad <68849516+shahrazeahmad07@users.noreply.github.com>
2021-08-05 16:29:18 +0500

    Initial commit

---
+++ app/src/main/java/com/uet/slideshowapp/SliderItem.java
@@ -0,0 +1,14 @@
+package com.uet.slideshowapp;
+
+public class SliderItem {
+    private int image;
+
+    SliderItem(int image)
+    {
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+}
