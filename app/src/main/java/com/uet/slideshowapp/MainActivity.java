819456f0ea1a0395c90624260949738acef168a6
Shahzaib Ahmad <68849516+shahrazeahmad07@users.noreply.github.com>
2021-08-07 12:41:41 +0500

    my functionality extracted.

--- app/src/main/java/com/uet/slideshowapp/MainActivity.java
+++ app/src/main/java/com/uet/slideshowapp/MainActivity.java
@@ -1,9 +1,15 @@
 package com.uet.slideshowapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +17,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager2 viewPager2;
+    private Handler sliderHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +35,33 @@ public class MainActivity extends AppCompatActivity {
 
         viewPager2.setAdapter(new SliderAdapter(sliderItems, viewPager2));
 
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                sliderHandler.removeCallbacks(sliderRunnable);
+                sliderHandler.postDelayed(sliderRunnable,3000);
+            }
+        });
+    }
+
+    private Runnable sliderRunnable = new Runnable() {
+        @Override
+        public void run() {
+            viewPager2.setCurrentItem(viewPager2.getCurrentItem() + 1);
+        }
+    };
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sliderHandler.removeCallbacks(sliderRunnable);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sliderHandler.postDelayed(sliderRunnable, 3000);
     }
 }
--- app/src/main/java/com/uet/slideshowapp/SliderAdapter.java
+++ app/src/main/java/com/uet/slideshowapp/SliderAdapter.java
@@ -37,6 +37,9 @@ public class SliderAdapter extends RecyclerView.Adapter<SliderAdapter.SliderView
     @Override
     public void onBindViewHolder(@NonNull  SliderAdapter.SliderViewHolder holder, int position) {
         holder.setImage(sliderItems.get(position));
+        if (position == sliderItems.size() - 2) {
+            viewPager2.post(runnable);
+        }
     }
 
     @Override
@@ -58,4 +61,12 @@ public class SliderAdapter extends RecyclerView.Adapter<SliderAdapter.SliderView
         }
 
     }
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            sliderItems.addAll(sliderItems);
+            notifyDataSetChanged();
+        }
+    };
 }
